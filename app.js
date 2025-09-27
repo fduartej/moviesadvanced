db766a27999281c96693e66bf46c75076a3ee13b
Fred Duarte <fduartej@gmail.com>
2025-09-27 09:22:36 -0500

    cambios react

--- app.js
+++ app.js
@@ -10,9 +10,3 @@ function MyApp() {
     </div>
   );
 }
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <MyApp />
-  </React.StrictMode>
-);
